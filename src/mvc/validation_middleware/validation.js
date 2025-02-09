6049fc0ec555da50c0a07c155cb45080dd5e0a74
sarita595k <sarita595k@gmail.com>
2025-02-09 21:20:34 +0530

    validation started

--- src/mvc/validation_middleware/validation.js
+++ src/mvc/validation_middleware/validation.js
@@ -1,5 +1,5 @@
 const { body, validationResult } = require('express-validator')
-
+// validation started here 
 const validationUserRegistration = (req, res, next) => [
     body('firstName').isLength({ min: 3 }).withMessage("name should be atleast 3 characters"),
 
